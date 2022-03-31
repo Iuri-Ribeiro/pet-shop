766709bea3941dfa968e5a8a1a28a90ae1ad179e
Iuri Dias Ribeiro <iuri.dias968@gmail.com>
2022-03-31 18:21:24 -0300

    Removendo o component card-produto

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -8,7 +8,6 @@ import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
 import { HeaderComponent } from 'src/app/components/header/header.component';
-import { CardProdutoComponent } from 'src/app/components/card-produto/card-produto.component';
 
 @NgModule({
   imports: [
@@ -17,6 +16,6 @@ import { CardProdutoComponent } from 'src/app/components/card-produto/card-produ
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage, HeaderComponent, CardProdutoComponent]
+  declarations: [HomePage, HeaderComponent]
 })
 export class HomePageModule { }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+    selector: 'app-home',
+    templateUrl: './home.page.html',
+    styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+    title: String = "Ração Pedrigree";
+    subtitle: String = "Ração de 18kg da pedigree para cachorros adultos";
+    price: String = "R$ 70,00";
+    image: String = "../../../assets/img/produtos/racao.jpg";
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
 
 }
