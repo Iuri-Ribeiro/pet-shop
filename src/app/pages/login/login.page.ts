2d69c2057e8e9a5f754fae7539f3e721f23d42a6
Iuri Dias Ribeiro <iuri.dias968@gmail.com>
2022-03-31 20:53:53 -0300

    Login com usuario mockado

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,17 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+    selector: 'app-login',
+    templateUrl: './login.page.html',
+    styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
 
-  image = "../../../assets/img/logo_size_invert.jpg";
+    email: String;
+    senha: String;
 
-  constructor() { }
+    usuarios = [
+        {
+            email: "iuri@gmail.com",
+            senha: "1234"
+        },
+        {
+            email: "maria@gamil.com",
+            senha: "4321"
+        }
+    ]
 
-  ngOnInit() {
-  }
+    image = "../../../assets/img/logo_size_invert.jpg";
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    login() {
+        this.usuarios.forEach(usuario => {
+            if (this.email == usuario.email && this.senha == usuario.senha) {
+                console.log("Logou");
+            }
+        })
+    }
 
 }
