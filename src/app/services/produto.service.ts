2f59aad01d808d5d7371a40d6ed51933b19c0a1c
Iuri Dias Ribeiro <iuri.dias968@gmail.com>
2022-04-07 13:52:02 -0300

    Criando produto service

---
+++ src/app/services/produto.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Produto } from '../models/produto.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutoService {
+
+  public produtos: Array<Produto> = [
+    {
+      id: 0,
+      title: "Ração Pedrigree",
+      subtitle: "Ração de 18kg da pedigree para cachorros adultos",
+      price: "R$ 192,59",
+      image: "../../../assets/img/produtos/racao.jpg"
+    },
+    {
+      id: 1,
+      title: "Ração Golden",
+      subtitle: "Ração de 10,1Kg da Golden para gatos filhotes, sabor frango",
+      price: "R$ 157,90",
+      image: "../../../assets/img/produtos/racao_gato.png"
+    },
+    {
+      id: 2,
+      title: "Ração Quatree",
+      subtitle: "Ração Quatree Gourmet para cachorros adultos de raças médias e grandes",
+      price: "R$ 42,99",
+      image: "../../../assets/img/produtos/racao2.jpg"
+    }
+  ]
+
+  constructor() { }
+}
