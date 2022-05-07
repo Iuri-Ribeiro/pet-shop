96f23be9c04d2e67ece4058f93fed1732df71689
Iuri <iuri.dias968@gmail.com>
2022-05-07 16:17:06 -0300

    Atualizando models

--- src/app/models/cliente.model.ts
+++ src/app/models/cliente.model.ts
@@ -1,23 +1,24 @@
 import { Endereco } from "./endereco.model";
 
 export class Cliente{
-    public id: number;
-    public nome: String;
-    public email: String;
-    public senha: String;
+    public id: string;
+    public nome: string;
+    public email: string;
+    public senha: string;
+    public cpf: string;
     public enderecos: Array<Endereco>;
 
     constructor()
-    constructor(id?: number,
-        nome?: String,
-        email?: String,
-        senha?: String,
+    constructor(id?: string,
+        nome?: string,
+        email?: string,
+        senha?: string,
         enderecos?: Array<Endereco>) {
 
-        this.id = id ? id : 0;
+        this.id = id ? id : '';
         this.nome = nome ? nome : '';
         this.email = email ? email : '';
         this.senha = senha ? senha : '';
-        this.enderecos = enderecos ? enderecos : [];
+        this.enderecos = enderecos ? enderecos : [];    
     }
 }
--- src/app/models/endereco.model.ts
+++ src/app/models/endereco.model.ts
@@ -1,18 +1,31 @@
 export class Endereco{
-    public cep: String;
-    public numero: String;
-    public logradouro: String;
-    public complemento: String;
+    public id: string
+    public cep: string;
+    public numero: string;
+    public logradouro: string;
+    public complemento: string;
+    public bairro: string;
+    public cidade: string;
+    public estado: string;
 
     constructor()
-    constructor(cep?: String,
-        numero?: String,
-        logradouro?: String,
-        complemento?: String) {
+    constructor(
+        id?: string,
+        cep?: string,
+        numero?: string,
+        logradouro?: string,
+        complemento?: string,
+        bairro?: string,
+        cidade?: string,
+        estado?: string) {
 
+        this.id = id ? id : '';
         this.cep = cep ? cep : '';
         this.numero = numero ? numero : '';
         this.logradouro = logradouro ? logradouro : '';
         this.complemento = complemento ? complemento : '';
+        this.bairro = bairro ? bairro : '';
+        this.cidade = cidade ? cidade : '';
+        this.estado = estado ? estado : '';
     }
 }
