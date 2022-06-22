4a7e62ca4b0adb814c7c9e8309ae11d360ee9ab8
Iuri Ribeiro <iuri.dias968@gmail.com>
2022-06-22 20:30:09 -0300

    Produto detalhes completo

--- src/app/pages/produto-detalhes/produto-detalhes.page.ts
+++ src/app/pages/produto-detalhes/produto-detalhes.page.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Produto } from 'src/app/models/produto.model';
 import { ProdutoService } from 'src/app/services/produto.service';
+import { Storage } from '@capacitor/storage';
+import { Cliente } from 'src/app/models/cliente.model';
+import { AlertController } from '@ionic/angular';
 
 @Component({
     selector: 'app-produto-detalhes',
@@ -11,15 +14,80 @@ import { ProdutoService } from 'src/app/services/produto.service';
 export class ProdutoDetalhesPage implements OnInit {
 
     public produto: Produto = new Produto();
+    public produtoEdit: Produto = new Produto();
+    public cliente;
+    public admin: boolean = false;
+    public isEditar = false;
 
-    constructor(private rotaAtiva: ActivatedRoute, private produtoService: ProdutoService) { }
+    constructor(
+        private rotaAtiva: ActivatedRoute,
+        private produtoService: ProdutoService,
+        private rota: Router,
+        private alerta: AlertController) { }
 
-    ngOnInit() {
+    async ngOnInit() {
         const codigo: string = this.rotaAtiva.snapshot.paramMap.get('id');
 
         this.produtoService.getById(codigo).then((produto) => {
             this.produto = produto;
         });
+
+        const resp = await Storage.get({ key: 'meu-usuario' });
+        this.cliente = JSON.parse(resp.value);
+
+        if (this.cliente)
+            this.admin = this.cliente.admin;
     }
 
+    public editar() {
+        this.isEditar = true;
+        this.produtoEdit.id = this.produto.id;
+        this.produtoEdit.title = this.produto.title;
+        this.produtoEdit.price = this.produto.price;
+        this.produtoEdit.subtitle = this.produto.subtitle;
+        this.produtoEdit.image = this.produto.image;
+        this.produtoEdit.categoria = this.produto.categoria;
+    }
+
+    public salvar() {
+        this.produtoService.edit(this.produtoEdit).then((resposta) => {
+            this.isEditar = false;
+            this.rota.navigate(['produto-detalhes', this.produto.id]);
+        });
+    }
+
+    public deletar() {
+        this.produtoService.delete(this.produto.id).then(() => {
+            this.rota.navigate(['/tabs/home']);
+        });
+    }
+
+    public async abrirAlerta() {
+        const alert = await this.alerta.create({
+            cssClass: 'my-custom-class',
+            header: 'Deletar produto',
+            subHeader: '',
+            message: 'Você realmente deseja deletar este produto?',
+            buttons: [
+                {
+                    text: 'Cancelar',
+                    role: 'cancel',
+                    cssClass: 'secondary',
+                    id: 'cancel-button',
+                    handler: (blah) => {
+                        console.log('Confirm Cancel: blah');
+                    }
+                },
+                {
+                    text: 'Ok',
+                    id: 'confirm-button',
+                    handler: () => {
+                        this.deletar();
+                    }
+                }
+            ]
+        });
+
+        await alert.present();
+    }
 }
--- src/app/services/produto.service.ts
+++ src/app/services/produto.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore} from '@angular/fire/compat/firestore';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 
 import { Produto } from '../models/produto.model';
@@ -12,22 +12,22 @@ export class ProdutoService {
 
     constructor(private firestore: AngularFirestore) { }
 
-    public getAll(){
+    public getAll() {
         return this.firestore.collection('produtos').snapshotChanges();
     }
 
     public getById(id: string): Promise<Produto> {
         return this.firestore.collection('produtos').doc(id).ref.get().then((item) => {
-            if(item.exists){
+            if (item.exists) {
                 const produto = item.data();
 
                 return {
                     id: item.id,
                     title: produto['title'],
                     subtitle: produto['subtitle'],
-                    price: produto['price'] ,
+                    price: produto['price'],
                     image: produto['image'],
-                    categoria: produto['categoira'],
+                    categoria: produto['categoria'],
                 };
             }
 
@@ -35,20 +35,20 @@ export class ProdutoService {
         });
     }
 
-    public add(produto: Produto): Promise<unknown>{
+    public add(produto: Produto): Promise<unknown> {
         delete produto.id;
         return this.firestore.collection('produtos').add({
             ...produto
         });
     }
 
-    public edit(produto: Produto): Promise<unknown>{
+    public edit(produto: Produto): Promise<unknown> {
         return this.firestore.doc(`produtos/${produto.id}`).update({
             ...produto
         });
     }
 
-    public delete(id: string): Promise<void>{
+    public delete(id: string): Promise<void> {
         return this.firestore.doc(`produtos/${id}`).delete();
     }
 }
